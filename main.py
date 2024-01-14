ce0c841d677f83e24e17a27cc5f1354413724103
TomaszJelinski <18537631-TomaszJelinski@users.noreply.replit.com>
2024-01-14 15:31:29 +0000

    działania i wyświetlanie

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 def dodawanie(a, b):
 	return a + b
 
-def odejmowanie
+def odejmowanie(a, b):
+	return a - b
+
+def mnozenie(a, b):
+	return a * b
+
+def dzielenie(a, b):
+	return a / b if b != 0 else "nie dziel przez zero"
+
+print("kalkulator")
+print("1. dodawanie")
+print("2. odejmowanie")
+print("3. mnozenie")
+print("4. dzielenie")
